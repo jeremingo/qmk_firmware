206aa47cfd73eb614b6c9416977657c5c9b5475d
jeremingo <58335558+jeremingo@users.noreply.github.com>
2020-12-31 16:04:34 +0200

    formatting keymap

--- keyboards/niu_mini/keymaps/jeremingo/keymap.c
+++ keyboards/niu_mini/keymaps/jeremingo/keymap.c
@@ -30,38 +30,38 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12(
-  KC_TAB,  DV_QUOT,    DV_COMM,    DV_DOT,    DV_P,    DV_Y,    DV_F,    DV_G,    DV_C,    DV_R,    DV_L,    KC_BSPC,
-  LCTL_T(KC_CAPS),  DV_A,    DV_O,    DV_E,    DV_U,    DV_I,    DV_D,    DV_H,    DV_T,    DV_N,    DV_S, DV_MINS,
-  KC_LSFT, DV_SCLN,    DV_Q,    DV_J,    DV_K,    DV_X,    DV_B,    DV_M,    DV_W, DV_V,  DV_Z, KC_ENT ,
-  KC_LGUI, KC_LCTL, RCTL_T(KC_ESC), KC_LALT, LSFT_T(KC_SPC), MO(_LOWER), MO(_RAISE),   RAISE_SPC,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_TAB,          DV_QUOT, DV_COMM,        DV_DOT,  DV_P,           DV_Y,       DV_F,       DV_G,      DV_C,    DV_R,    DV_L,  KC_BSPC,
+  LCTL_T(KC_CAPS), DV_A,    DV_O,           DV_E,    DV_U,           DV_I,       DV_D,       DV_H,      DV_T,    DV_N,    DV_S,  DV_MINS,
+  KC_LSFT,         DV_SCLN, DV_Q,           DV_J,    DV_K,           DV_X,       DV_B,       DV_M,      DV_W,    DV_V,    DV_Z,  KC_ENT,
+  KC_LGUI,         KC_LCTL, RCTL_T(KC_ESC), KC_LALT, LSFT_T(KC_SPC), MO(_LOWER), MO(_RAISE), RAISE_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 [_DVORAK] = LAYOUT_ortho_4x12(
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
-  LCTL_T(KC_CAPS),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT ,
-  KC_LGUI, KC_LCTL, RCTL_T(KC_ESC), KC_LALT, LSFT_T(KC_SPC), MO(_LOWER), MO(_RAISE),   RAISE_SPC,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_TAB,          KC_QUOT, KC_COMM,        KC_DOT,  KC_P,           KC_Y,       KC_F,       KC_G,      KC_C,    KC_R,    KC_L,  KC_BSPC,
+  LCTL_T(KC_CAPS), KC_A,    KC_O,           KC_E,    KC_U,           KC_I,       KC_D,       KC_H,      KC_T,    KC_N,    KC_S,  KC_SLSH,
+  KC_LSFT,         KC_SCLN, KC_Q,           KC_J,    KC_K,           KC_X,       KC_B,       KC_M,      KC_W,    KC_V,    KC_Z,  KC_ENT,
+  KC_LGUI,         KC_LCTL, RCTL_T(KC_ESC), KC_LALT, LSFT_T(KC_SPC), MO(_LOWER), MO(_RAISE), RAISE_SPC, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_ortho_4x12(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, _______,
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PIPE, _______, _______,
-  _______, _______, _______, _______, _______,     _______, _______,      _______,    KC_PSCR,    KC_BRID, KC_BRIU, LALT(KC_F4)
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_BRID, KC_BRIU, LALT(KC_F4)
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGUP, KC_PGDN, KC_BSLS, _______, _______,
-  _______, _______, _______, _______, _______,     _______, _______,      _______, TD(TD_NXT_PRV), KC_VOLD, KC_VOLU, TD(TD_PLY_MUTE)
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,           KC_9,    KC_0,    KC_BSPC,
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,         KC_LBRC, KC_RBRC, _______,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGUP, KC_PGDN,        KC_BSLS, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, TD(TD_NXT_PRV), KC_VOLD, KC_VOLU, TD(TD_PLY_MUTE)
 ),
 
 [_ADJUST] = LAYOUT_ortho_4x12(
-  LALT(LCTL(KC_DEL)), RESET,   DEBUG, _______,  _______,  _______, _______, _______, _______,_______, _______, KC_DEL ,
-  _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, LAYOUT,  _______, _______,  _______,  _______,
-  _______, _______,  _______,  _______,   _______,  _______,   _______,  _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______,     _______, _______,      _______, _______, _______, _______, _______
+  LALT(LCTL(KC_DEL)), RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______,            RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, LAYOUT,  _______, _______, _______, _______,
+  _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 
