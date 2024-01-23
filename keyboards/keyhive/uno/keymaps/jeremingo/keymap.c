ad1e8f8272e047193a88a97490e7ad69ed0ac544
jeremingo <58335558+jeremingo@users.noreply.github.com>
2024-01-23 18:34:46 +0200

    extract functions

--- keyboards/keyhive/uno/keymaps/jeremingo/keymap.c
+++ keyboards/keyhive/uno/keymaps/jeremingo/keymap.c
@@ -1,6 +1,9 @@
 #include QMK_KEYBOARD_H
 #include "../../../../../quantum/keymap_extras/keymap_dvorak.h"
 
+void register_arch(void);
+void unregister_arch(void);
+
 enum uno_keycode
 {
   ARCH = SAFE_RANGE
@@ -21,23 +24,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ARCH:
       if (record->event.pressed) {
-        if (is_arch_open) {
-          tap_code16(RCTL(DV_M));
-        } else {
-          tap_code16(LGUI(DV_R));
-          wait_ms(200);
-          
-          tap_code(DV_A);
-          tap_code(DV_R);
-          tap_code(DV_C);
-          tap_code(DV_H);
-        }
-      } else if (is_arch_open) {
-        is_arch_open = false;
+        register_arch();
       } else {
-        wait_ms(170);
-        tap_code(KC_ENTER);
-        is_arch_open = true;
+        unregister_arch();
       }
       break;
       return false;
@@ -50,3 +39,26 @@ void keyboard_post_init_user(void) {
   rgblight_sethsv_noeeprom(HSV_GREEN);
 }
 
+void register_arch() {
+  if (is_arch_open) {
+    tap_code16(RCTL(DV_M));
+  } else {
+    tap_code16(LGUI(DV_R));
+    wait_ms(200);
+    
+    tap_code(DV_A);
+    tap_code(DV_R);
+    tap_code(DV_C);
+    tap_code(DV_H);
+  }
+}
+
+void unregister_arch() {
+  if (is_arch_open) {
+    is_arch_open = false;
+  } else {
+    wait_ms(170);
+    tap_code(KC_ENTER);
+    is_arch_open = true;
+  }
+}
